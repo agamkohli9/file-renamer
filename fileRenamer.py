c14a4813c55b6b9fed5099ca2f172b3cf2064978
agamkohli9 <agamkohli9@gmail.com>
2021-01-20 15:47:05 +0000

    bug fixes

--- fileRenamer.py
+++ fileRenamer.py
@@ -9,31 +9,35 @@ order = ('front', 'back', 'label', 'right', 'left')
 #read in data from csv file
 sheet = pd.read_csv('sheet.csv')
 
-#input starting and ending image numbers
-begin = input('starting number: ')
-end = input('ending number: ')
-
-confirmation_str = 'renaming files IMG_' + begin + '.CR2' + ' to ' + 'IMG_' + end + '.CR2 (y/n)'
-confirmation = input(confirmation_str)
-if confirmation == 'n':
-    sys.exit()
-
 #get directory of files
-dir = input('enter images directory (should look like /Volumes/name_of_device/something')
+dir = input('enter images directory (should look like /Volumes/name_of_device/something): ')
 
 print('Loading...')
 
-#rename files
-end = int(end)
-begin = int(begin)
+#make renamedImages directory
+command = ('mkdir', dir + '/../renamedImages')
+subprocess.call(command)
 
-for i in range(end - begin + 1):
+#rename files
+command = ('ls', dir)
+pipe = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+command = ('wc', '-l')
+length = int(subprocess.Popen(command, stdin=pipe.stdout, stdout=subprocess.PIPE).stdout.read())
+
+command = ('ls', dir)
+
+for i in range(length):
     #set file names
-    i_file = dir + 'IMG_' + str(i + begin) + '.CR2'
-    o_file = dir + sheet.loc[i, 'code'] + '.' + order[i % 5]
+    i_file = dir + '/' + str(subprocess.Popen(command, stdout=subprocess.PIPE).stdout.read())[2:14]
+    o_file = dir + '/../renamedImages/' + sheet.loc[int((i + len(order)) / len(order)) - 1, 'code'] + '.' + order[i % len(order)]
 
     #do unix command to change file name
     subprocess.call(['mv', i_file, o_file])
+
+#delete empty directory
+command = ('rmdir', dir)
+subprocess.call(command)
 
 #let user know program is finished
 print('Done!')
